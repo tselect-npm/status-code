ace636bd6afb772eec3d2ffbb3805232e1fdfd77
Sylvain Estevez <estevez.sylvain@gmail.com>
2017-10-03 11:11:41 -0700

    :bug: Fixed unit tests

--- src/utils/is-non-error-status-code.ts
+++ src/utils/is-non-error-status-code.ts
@@ -1,6 +1,7 @@
 import { TStatusCode } from '../types/status-code';
 import { isErrorStatusCode } from './is-error-status-code';
+import { isStatusCode } from './is-status-code';
 
 export function isNonErrorStatusCode(code: TStatusCode): boolean {
-  return !isErrorStatusCode(code);
+  return isStatusCode(code) && !isErrorStatusCode(code);
 }
